74c39dbf491a3fd0518173e2b3ff0667ef5a33fb
Emre Ç <superbevent@hotmail.com>
2022-01-13 11:48:05 +0300

    Pagination performed in Book/Index.. With a minor bug!

--- LoanBookDB_ASPNET_MVC_Web/Controllers/BookController.cs
+++ LoanBookDB_ASPNET_MVC_Web/Controllers/BookController.cs
@@ -11,12 +11,17 @@ namespace LoanBookDB_ASPNET_MVC_Web.Controllers
     public class BookController : Controller
     {
         BookManager bookManager = new BookManager();
+        public const int pageSize = 10;
         // GET: Book
-        public ActionResult Index()
+        public ActionResult Index(int? page=1)
         {
             try
             {
-                List<Book> bookList = bookManager.BringAllActiveBooks();
+                List<Book> bookList = bookManager.BringAllActiveBooks().Skip((page.Value < 1 ? 1 : page.Value -1) * pageSize).Take(pageSize).ToList();
+                var total = bookManager.BringAllActiveBooks().Count();
+                ViewBag.TotalPages = (int)Math.Ceiling(total / (double)pageSize);
+                ViewBag.Now = page;
+                ViewBag.PageSize = pageSize;
                 ViewBag.BookListCount = 0;
                 if (bookList.Count > 0)
                 {
