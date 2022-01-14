dea864f51ee7df5e408e88925f6ee63e67b715e8
Emre Ç <superbevent@hotmail.com>
2022-01-14 15:02:14 +0300

    Partial View Implemented. Further improvements to be continued!

---
+++ LoanBookDB_ASPNET_MVC_Web/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoanBookDB_ASPNET_MVC_Web_BLL.Managers;
+
+namespace LoanBookDB_ASPNET_MVC_Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        BookManager bookManager = new BookManager();
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            ViewBag.BooksAddedToday = bookManager.BringAllActiveBooks().Where(x => x.DateRegistered > DateTime.Now.AddDays(-1)
+            && x.DateRegistered < DateTime.Now.AddDays(1)).Count();
+            return View();
+        }
+    }
+}
---
+++ LoanBookDB_ASPNET_MVC_Web/Controllers/PartialsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoanBookDB_ASPNET_MVC_Web_BLL.Managers;
+
+namespace LoanBookDB_ASPNET_MVC_Web.Controllers
+{
+    public class PartialsController : Controller
+    {
+        BookManager bookManager = new BookManager();
+        //To manage all partials in one place we created partial controller. You can create partial controller inside any of your controllers
+        //if necessary
+
+        public PartialViewResult PartialMenuResult()
+        {
+            //TODO: 
+            int totalBooks = bookManager.BringAllActiveBooks().Count();
+            TempData["TotalBooks"] = totalBooks;
+            return PartialView("_PartialMenu");
+        }
+    }
+}
